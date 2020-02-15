53eca7746c8d89e38ef40b1d6987d7ad643cdf56
Marko Mitranić <marko.mitranic@gmail.com>
2020-02-15 19:11:14 +0100

    Finish a dummy frontend ws comms

--- frontend/src/scripts/test.js
+++ frontend/src/scripts/test.js
@@ -2,8 +2,28 @@
 class Test {
 
     echo() {
-        let socket = new WebSocket("ws://" + location.host + "/poker-endpoint/");
-        console.log(socket);
+        let socket = new WebSocket("ws://" + location.host + "/poker-entrypoint/");
+
+        socket.onopen = function(e) {
+            socket.send('Good morning Mr. Server!');
+        };
+
+        socket.onmessage = function(event) {
+            console.log('[incoming message]', event.data);
+        };
+
+        socket.onclose = function(event) {
+            if (event.wasClean) {
+                console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+            } else {
+                console.log('[socket close] Server left.');
+            }
+        };
+
+        socket.onerror = function(error) {
+            console.error(`[socket error] ${error.message}`);
+        };
+
     }
 
 }
