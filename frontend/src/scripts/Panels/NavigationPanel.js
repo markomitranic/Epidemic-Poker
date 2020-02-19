ba0539ee7d8cb93dc16cf2218563d2e4c015fb6b
Marko Mitranić <marko.mitranic@gmail.com>
2020-02-19 12:10:20 +0100

    Add joined rooms to navigation panel

--- frontend/src/scripts/Panels/NavigationPanel.js
+++ frontend/src/scripts/Panels/NavigationPanel.js
@@ -3,17 +3,40 @@ class NavigationPanel {
 
     constructor(panelManager) {
         this.panelManager = panelManager;
+        this.roomList = [];
         this.bootstrapDom();
     }
 
     bootstrapDom() {
         this.element = document.querySelector('#navigation-panel');
         this.button = this.element.querySelector('.toggle-button');
+        this.roomListContainer = this.element.querySelector('ul.rooms-list');
 
         this.element.navigationPanel = this;
         this.button.navigationPanel = this;
         this.button.addEventListener('click', this.togglePanel);
         this.panelManager.register(this);
+    }
+
+    addRoom(room) {
+        if (this.roomList) {
+            for(let i=0; i < this.roomList.length; i++) {
+                if (this.roomList[i].roomObject === room) {
+                    return;
+                }
+            }
+        }
+
+        const newItem = document.createElement('li');
+        newItem.innerText = room.name.charAt(0).toUpperCase() + room.name.slice(1);
+        newItem.roomObject = room;
+        this.roomList.push(newItem);
+        //listener
+        this.roomListContainer.appendChild(newItem);
+    }
+
+    removeRoom(room) {
+
     }
 
     togglePanel() {
@@ -26,12 +49,6 @@ class NavigationPanel {
 
     open() {
         this.panelManager.open(this);
-    }
-
-    showError(message)
-    {
-        this.errorMessage.innerText = message;
-        this.errorMessage.classList.add('visible');
     }
 
 }
