ff36174a924e3d928692b3e552037b1a42734e54
Marko Mitranić <marko.mitranic@gmail.com>
2020-02-19 21:54:14 +0100

    Fix up the join page with some styles

--- frontend/src/scripts/Panels/JoinPopup.js
+++ frontend/src/scripts/Panels/JoinPopup.js
@@ -11,17 +11,20 @@ class JoinPopup {
 
     bootstrapDom() {
         this.element = document.querySelector('#join-popup');
-        this.button = this.element.querySelector('button');
-        this.gsInput = this.element.querySelector('#join-gs-name');
-        this.roomInput = this.element.querySelector('#join-room-name');
+        this.form = this.element.querySelector('form');
+        this.button = this.form.querySelector('button');
+        this.gsInput = this.form.querySelector('#join-gs-name');
+        this.roomInput = this.form.querySelector('#join-room-name');
 
         this.element.joinPopup = this;
         this.button.joinPopup = this;
         this.button.addEventListener('click', this.submitJoinRequest);
+        this.form.addEventListener('submit', this.submitJoinRequest);
         this.popupManager.register(this);
     }
 
-    submitJoinRequest() {
+    submitJoinRequest(e) {
+        e.preventDefault();
         const values = [this.joinPopup.gsInput.value, this.joinPopup.roomInput.value];
         if (values.length !== 2 || !values[0].length || !values[1].length) {
             new ErrorMessage('Both fields must be filled if you want us to find the room.');
