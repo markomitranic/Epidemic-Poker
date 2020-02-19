234bfe2238eea6ab2c0361239ae0875a41f1f58a
Marko Mitranić <marko.mitranic@gmail.com>
2020-02-19 21:40:31 +0100

    Extract shareControls as a separate service

--- frontend/src/scripts/Poker/Room/Window/RoomWindow.js
+++ frontend/src/scripts/Poker/Room/Window/RoomWindow.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import VoteControls from "./VoteControls";
+import ShareControls from "./ShareControls";
 
 class RoomWindow {
 
@@ -10,14 +11,12 @@ class RoomWindow {
 
     bootstrapDom() {
         this.main = document.querySelector('main');
-        this.noRoomWarning = this.main.querySelector('#no-rooms');
         this.roomWindow = this.main.querySelector('#room-window');
         this.roomTitle = this.roomWindow.querySelector('h1 span');
         this.votingButtonsContainer = this.roomWindow.querySelector('.voting-buttons');
         this.voteControls = new VoteControls(this.votingButtonsContainer);
         this.userActions = this.roomWindow.querySelector('.user-actions');
-        this.userActions.shareableLink = this.userActions.querySelector('.shareable-link span');
-        this.userActions.shareableCodeBlocks = this.userActions.querySelectorAll('.shareable-code span');
+        this.shareControls = new ShareControls(this.userActions.querySelector('.share'));
         this.userActions.coffeeButton = this.userActions.querySelector('.coffee-break');
         this.userActions.leaveButton = this.userActions.querySelector('.leave-room');
         this.visualContainer = this.roomWindow.querySelector('#visual');
@@ -30,9 +29,7 @@ class RoomWindow {
     populateData(room) {
         this.roomTitle.innerText = room.name.charAt(0).toUpperCase() + room.name.slice(1);
         this.voteControls.initialize(room);
-        this.userActions.shareableLink.innerText = 'http://localhost:8080/kyoto/abercrombie';
-        this.userActions.shareableCodeBlocks[0].innerText = room.connection.serverName;
-        this.userActions.shareableCodeBlocks[1].innerText = room.name;
+        this.shareControls.populate(room);
         this.main.classList.add('room-open');
     }
 
--- frontend/src/scripts/Poker/Room/Window/ShareControls.js
+++ frontend/src/scripts/Poker/Room/Window/ShareControls.js
@@ -2,8 +2,17 @@
 
 class ShareControls {
 
-    constructor(container, room) {
+    constructor(container) {
+        this.shareableLink = container.querySelector('.shareable-link span');
+        this.shareableCodeBlocks = container.querySelectorAll('.shareable-code span');
+    }
 
+    populate(room) {
+        this.shareableLink.innerText = `http://${location.host}/${room.connection.serverName}/${room.name}`;
+        this.shareableCodeBlocks[0].innerText = room.connection.serverName;
+        this.shareableCodeBlocks[1].innerText = room.name;
     }
 
 }
+
+export default ShareControls;
