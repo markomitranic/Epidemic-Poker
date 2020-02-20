fd1934f16f6ea1108695bde9da58153882f447bb
Marko Mitranić <marko.mitranic@gmail.com>
2020-02-20 02:29:26 +0100

    Split deployment scripts

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -9,6 +9,6 @@ set +a
 # Pre-Build all the non-public container images.
 docker build ./frontend -t epidemicpoker_frontend --target ${ENV}
 # Run the thing.
-docker-compose down
-docker-compose build
-docker-compose up --remove-orphans
+docker-compose -f docker-compose.yml -f docker-compose-dev.yml down
+docker-compose -f docker-compose.yml -f docker-compose-dev.yml build
+docker-compose -f docker-compose.yml -f docker-compose-dev.yml up --remove-orphans
